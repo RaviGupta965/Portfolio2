bd57bd51bdf023f61cf6b5247284e3dede36550e
RaviGupta965 <clicktoravi1234@gmail.com>
2025-06-28 01:41:53 +0530

    content-compeleted

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const toggle = document.getElementById("menu-toggle");
-  const navItems = document.getElementById("nav-items");
+ const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".navbar a");
 
-  toggle.addEventListener("click", () => {
-    navItems.classList.toggle("show");
+  window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 200;
+      const sectionHeight = section.offsetHeight;
+      if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href").includes(current)) {
+        link.classList.add("active");
+      }
+    });
   });
