793e0b8ce0a96816fd10545f1384ae1279deb1bf
RaviGupta965 <clicktoravi1234@gmail.com>
2025-06-27 12:51:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("menu-toggle");
+  const navItems = document.getElementById("nav-items");
+
+  toggle.addEventListener("click", () => {
+    navItems.classList.toggle("show");
+  });
