9efe8c5001f36c6d75268f8fb4042672950867ed
RaviGupta965 <clicktoravi1234@gmail.com>
2025-06-30 13:11:49 +0530

    Mobile-nav-added

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
- const sections = document.querySelectorAll("section");
-  const navLinks = document.querySelectorAll(".navbar a");
+// toggle navbar-icon
 
-  window.addEventListener("scroll", () => {
-    let current = "";
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop - 200;
-      const sectionHeight = section.offsetHeight;
-      if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
-        current = section.getAttribute("id");
-      }
-    });
+let menuicon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
 
-    navLinks.forEach((link) => {
-      link.classList.remove("active");
-      if (link.getAttribute("href").includes(current)) {
-        link.classList.add("active");
-      }
-    });
+menuicon.onclick=()=>{
+  menuicon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 200;
+    const sectionHeight = section.offsetHeight;
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
   });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
